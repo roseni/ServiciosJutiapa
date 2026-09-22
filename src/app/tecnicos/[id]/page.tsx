e353137c3c582ba062cd11e351ec78e4714d0dc3
Roseni <rosenibarillas@gmail.com>
2026-09-21 22:15:56 -0600

    actualizacion 1

--- src/app/tecnicos/[id]/page.tsx
+++ src/app/tecnicos/[id]/page.tsx
@@ -190,7 +190,29 @@ export default function TechnicianPublicProfilePage() {
             </div>
           </div>
         </div>
-      
+         {/*  si no quiero mostrar el mapa quito eso */}
+        {/*  MAPA  */}
+      <div className="mb-6" style={{ height: "250px" }}>
+        <MapaTecnico />
+      </div>
+
+      {/* Ubicación del tecnico */}
+        <div className="flex items-center justify-between mb-4">
+            <h2 className="text-lg sm:text-xl font-semibold text-gray-900 flex items-center gap-2">
+              <svg className="w-5 h-5 sm:w-6 sm:h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+              </svg>
+                Dirección
+            </h2>
+            {!isEditing && (
+              <button
+                onClick={() => setIsEditing(true)}
+                className="px-3 py-1.5 text-sm font-medium text-gray-700 bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors active:scale-95 touch-manipulation"
+              >
+               
+              </button>
+            )}
+          </div>
           
           {/* Fotografía del DPI */}
           {technician.dpiFotoUrl && (
